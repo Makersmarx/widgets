34d7f73ddb2315546ef7348c4e50a219298dc2b8
Makersmarx <bbiehn1@gmail.com>
2020-09-16 19:25:21 -0600

    hooks practice

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+
+  console.log('I run with every render');
+
+  useEffect(() => {
+    console.log('I run after every render');
+  }, [term]);
+
   return (
     <div>
       <div className="ui form">
