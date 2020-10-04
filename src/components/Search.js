8225ef5bbbb0b16ee28a3d8d69854e9bfb945db9
Makersmarx <bbiehn1@gmail.com>
2020-10-04 17:55:37 -0600

    wikipedia search bar practice

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+  // console.log(results);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
@@ -15,19 +18,22 @@ const Search = () => {
           srsearch: term,
         },
       });
+
+      setResults(data.query.search);
     };
+
     search();
   }, [term]);
 
   return (
     <div>
-      <div className="ui form">
-        <div className="field">
+      <div className='ui form'>
+        <div className='field'>
           <label>Enter Search Term</label>
           <input
             value={term}
             onChange={(e) => setTerm(e.target.value)}
-            className="input"
+            className='input'
           />
         </div>
       </div>
